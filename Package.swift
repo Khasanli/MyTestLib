62da609b691307441322f979d812a3ed7fc62ab7
Khayala <khasanli2021@gmail.com>
2024-01-25 22:44:09 +0400

    ss

--- Package.swift
+++ Package.swift
@@ -6,26 +6,16 @@ import PackageDescription
 let package = Package(
     name: "MyTestLib",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "MyTestLib",
-            targets: ["MyTestLib"]),
-        .library(
-            name: "TestLibraryFramework",
-            targets: ["TestLibraryFramework"])
+        .library(name: "MyTestLib", targets: ["MyTestLib"])
+    ],
+    dependencies: [
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MyTestLib"),
         .binaryTarget(
-                   name: "TestLibraryFramework",
-                   url: "https://github.com/Khasanli/PackageZip/raw/main/TestLibraryFramework.xcframework.zip",
-                   checksum: "9a71d8de27dbaa3d081a4932624792103bdfd5b6879e06afe969028cbde6ace3"
-               ),
-        .testTarget(
-            name: "MyTestLibTests",
-            dependencies: ["MyTestLib"]),
+            name: "TestLibraryFramework",
+            url: "https://github.com/Khasanli/PackageZip/raw/main/TestLibraryFramework.xcframework.zip",
+            checksum: "9a71d8de27dbaa3d081a4932624792103bdfd5b6879e06afe969028cbde6ace3"
+        )
     ]
 )
+
