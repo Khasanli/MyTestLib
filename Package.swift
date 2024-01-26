1f57b44e760d292fab1b40e9b8cff92fdd93c975
Khayala <khasanli2021@gmail.com>
2024-01-26 09:13:40 +0400

    test

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// testing
 import PackageDescription
 
 let package = Package(
