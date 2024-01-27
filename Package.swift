34b331f72d956d27cdc882457b3dabf8d98427fa
Khayala <khasanli2021@gmail.com>
2024-01-27 14:12:52 +0400

    back to future

--- Package.swift
+++ Package.swift
@@ -3,36 +3,18 @@
 // testing
 import PackageDescription
 
-//let package = Package(
-//    name: "MyTestLib",
-//    products: [
-//        .library(name: "MyTestLib", targets: ["MyTestLib"])
-//    ],
-//    dependencies: [
-//    ],
-//    targets: [
-//        .target(name: "MyTestLib",
-//               dependencies: ["TestLibraryFramework"]),
-//        .binaryTarget(name: "TestLibraryFramework", path: "./Sources/TestLibraryFramework.xcframework"),
-//        
-//    ]
-//)
-//
-
 let package = Package(
     name: "MyTestLib",
     products: [
-        .library(
-            name: "MyTestLib",
-            targets: ["MyTestLib", "TestLibraryFramework"]
-        )
+        .library(name: "MyTestLib", targets: ["MyTestLib"])
+    ],
+    dependencies: [
     ],
     targets: [
         .target(name: "MyTestLib",
-                       dependencies: ["TestLibraryFramework"]),
-        .binaryTarget(
-            name: "TestLibraryFramework",
-            path: "./Sources/TestLibraryFramework.xcframework"
-        )
+               dependencies: ["TestLibraryFramework"]),
+        .binaryTarget(name: "TestLibraryFramework", path: "./Sources/TestLibraryFramework.xcframework"),
+        
     ]
 )
+
