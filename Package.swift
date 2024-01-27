8f27ad5aecfaa497b79dbd76ec3dadde3366b474
Khayala <khasanli2021@gmail.com>
2024-01-27 14:49:19 +0400

    tested

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "MyTestLib", path: "./Sources/TestLibraryFramework.xcframework"),
+        .binaryTarget(name: "MyTestLib", path: "./Sources/MyTestLib.xcframework"),
     ]
 )
 
